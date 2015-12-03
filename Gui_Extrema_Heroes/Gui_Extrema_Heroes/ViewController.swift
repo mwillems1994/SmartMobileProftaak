c09030e5341c5aeb9745336cc290fd958a5bf370
KevinBorghmans <kevin_borghmans@hotmail.com>
2015-12-03 10:38:00 +0100

    Gui
    Gui uitwerking _ Macro’s moeder

---
+++ Gui_Extrema_Heroes/Gui_Extrema_Heroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gui_Extrema_Heroes
+//
+//  Created by Kevin Borghmans on 03/12/15.
+//  Copyright © 2015 Kevin Borghmans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
