4491e5b5201660c5158bcb523bc2a1e531a92947
KevinBorghmans <kevin_borghmans@hotmail.com>
2015-12-04 21:44:07 +0100

    update GUI
    Nog steeds bezig met GUI weer ln vedergeval een stuk verder

--- Gui_App_Extrema_Heroes/Gui_App_Extrema_Heroes/ContentViewController.swift
+++ Gui_App_Extrema_Heroes/Gui_App_Extrema_Heroes/ContentViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ContentViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var pageIndex: Int!
+    
+    var titleText: String!
+    
+    var imageFile: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.imageView.image = UIImage(named: self.imageFile)
+        
+        self.titleLabel.text = self.titleText
 
 
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
