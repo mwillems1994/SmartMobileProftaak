59d36757a73706ae2937e8b648c31cac6685640d
KevinBorghmans <kevin_borghmans@hotmail.com>
2015-12-11 10:11:33 +0100

    New hud

---
+++ Extrema Heroes/SidebarMenu/RefferalViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RefferalViewController.swift
+//  Extrema Heroes
+//
+//  Created by Kevin Borghmans on 11/12/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RefferalViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
+        // Do any additional setup after loading the view.
+    }
+
+    func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
